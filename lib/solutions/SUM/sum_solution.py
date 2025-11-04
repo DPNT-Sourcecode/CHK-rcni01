4832768a4a85f41c2e026d0e6222213f613ece0e
sbx_user1051 <sbx_user1051@169.254.86.55>
2025-11-04 19:45:38 +0000

    Tue Nov 04 19:45:38 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,11 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, x: int, y: int) -> int:
+
+        # Both parameters should be between 0 and 100, if not raise a value error
+        if not (0 <= x <= 100 and 0 <= y <= 100):
+            raise ValueError("Both numbers should be between 0 and 100 inclusive")
+
+        return x + y
+
