0fc921544cea2d083b81ec4e1c236038823f7f3b
sbx_user1051 <sbx_user1051@169.254.62.81>
2025-11-05 19:34:58 +0000

    Wed Nov 05 19:34:58 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,19 @@
+from typing import Dict
 
 class CheckoutSolution:
 
     # skus = unicode string
-    def checkout(self, skus: list[str]):
-        raise NotImplementedError()
+    def checkout(self, skus: str) -> int:
+        
+        prices: Dict[str, int] = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15
+        }
+
+        
+
+        return -1
+
 
