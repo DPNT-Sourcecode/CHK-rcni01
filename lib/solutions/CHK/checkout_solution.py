e654b75aa6dcf0bf1c9ea5a6f7680f89a29b57e3
sbx_user1051 <sbx_user1051@169.254.56.241>
2025-11-06 15:51:14 +0000

    Thu Nov 06 15:51:14 UTC 2025

---
+++ lib/solutions/CHK/checkout_solution.py
@@ -0,0 +1,56 @@
+from typing import Dict, Tuple
+from collections import Counter
+import math
+
+class CheckoutSolution:
+
+    # CHK_R2 answer
+    def checkout(self, skus) -> int:
+        if type(skus) != str:
+            return -1
+        
+        prices: Dict[str, int] = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+            "E": 40,
+            "F": 10
+        }
+
+        for sku in skus:
+            if sku not in prices:
+                return -1
+
+        skuCounts: Counter[str] = Counter(skus)
+
+        total: int = 0
+
+        if "A" in skuCounts:
+            countA: int = skuCounts["A"]
+            firstOfferGroups: int = math.floor(countA / 5)
+            total = total + firstOfferGroups * 200
+            remainder: int = countA - firstOfferGroups * 5
+            if remainder >= 3:
+                total = total + 130
+                remainder = remainder - 3
+            total = total + remainder * 50                
+        
+        if "E" in skuCounts and "B" in skuCounts:
+            countE: int = skuCounts["E"]
+            freeB: int = math.floor(countE / 2)
+            skuCounts["B"] = max(0, skuCounts["B"] - freeB)
+        
+        if skuCounts["F"] >= 3:
+            
+
+        if "B" in skuCounts:
+            countB: int = skuCounts["B"]
+            offerGroups: int = math.floor(countB / 2)
+            remainder: int = countB - offerGroups * 2
+            total = total + offerGroups * 45 + remainder * 30
+
+        for sku in ["C", "D", "E"]:
+            total = total + skuCounts[sku] * prices[sku]
+
+        return total
---
+++ test/solution_tests/CHK/test_checkout_solution_R3.py
@@ -0,0 +1,10 @@
+from lib.solutions.CHK.checkout_solution import CheckoutSolution
+
+class TestCheckout():
+
+    def test_correct_return_for_incorrect_type(self):
+        assert CheckoutSolution().checkout(123) == -1
+
+    def test_correct_return_for_incorrect_letters(self):
+        assert CheckoutSolution().checkout("TGH") == -1
+        assert CheckoutSolution().checkout("AABCDFG") == -1
